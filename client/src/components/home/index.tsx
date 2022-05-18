13a5bcca434cd0fd00d12aa0c5c1031fa82792bb
Nguyen Trung <trungdipdo5@gmail.com>
2022-05-18 22:30:25 +0700

    update

--- client/src/components/home/index.tsx
+++ client/src/components/home/index.tsx
@@ -34,9 +34,9 @@ export const Home = () => {
           <Hero />
         </div>
       </div>
-      <div>
+      {/* <div>
         <Heading />
-      </div>
+      </div> */}
       <div>
         <ListHotels />
       </div>
